aaecae25f3024fddf57b80ce564bb0e4f0a0a2af
韩猛 <hanmeng@juxinli.com>
2018-07-04 15:12:39 +0800

    haha

--- app/src/main/java/com/hm/gitdemo/MainActivity.java
+++ app/src/main/java/com/hm/gitdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.hm.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView textView = new ImageView(this);
+        textView.setImageResource(R.mipmap.ic_launcher);
+        Button btn = new Button(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
