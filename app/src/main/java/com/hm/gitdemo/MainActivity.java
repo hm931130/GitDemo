e7cbb11f60ac0c29dff5932413b4b160d7195b61
韩猛 <hanmeng@juxinli.com>
2018-07-04 15:10:07 +0800

    img

--- app/src/main/java/com/hm/gitdemo/MainActivity.java
+++ app/src/main/java/com/hm/gitdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.hm.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView textView = new ImageView(this);
+        textView.setImageResource(R.mipmap.ic_launcher);
     }
 }
