f2e35e8740fcb66d08596b4bafd10b868415ba6d
韩猛 <hanmeng@juxinli.com>
2018-07-04 15:11:58 +0800

    xxx

--- app/src/main/java/com/hm/gitdemo/MainActivity.java
+++ app/src/main/java/com/hm/gitdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.hm.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView textView = new ImageView(this);
         textView.setImageResource(R.mipmap.ic_launcher);
+        Button btn = new Button(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
